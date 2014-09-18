9f8c3cc11bc53b192c649f56e5c9bbed93237437
Robert Osvath Boros <osvathrobi@yahoo.com>
2014-09-18 18:34:27 +0300

    - test

--- ibus_interface.py
+++ ibus_interface.py
@@ -1,6 +1,6 @@
 import serial, time, logging
 
-DEBUG = True
+DEBUG = False
 
 # LOCATIONS, a mapping of hex codes seen in SRC/DST parts of packets. This WILL change across models/years.
 LOCATIONS = {
